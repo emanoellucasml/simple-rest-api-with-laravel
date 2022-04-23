55496cbd77fb163ecad4cc64d48698727aaad745
Emanoel Lucas <emanoellucas123456@gmail.com>
2022-04-23 11:03:10 -0300

    creates seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\Store::factory()
+            ->hasProducts(15)
+            ->create();
     }
 }
