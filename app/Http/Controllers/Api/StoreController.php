9bdf1cd37a7f9daa6f8f1337d6b50e06bddeb6d3
Emanoel Lucas <emanoellucas123456@gmail.com>
2022-04-27 16:17:16 -0300

    creates update, store and destroy methods for StoreController

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Store;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
 
 class StoreController extends Controller
 {
@@ -15,22 +17,29 @@ class StoreController extends Controller
 
     public function store(Request $request)
     {
-
+        return Store::create($request->all());
     }
 
 
     public function show(Store $store)
     {
+        return $store->with('products')->get();
+    }
+
+    public function update(Request $request, Store $store)
+    {
+        $store->update($request->all());
         return $store;
     }
 
-    public function update(Request $request, $id)
+    public function destroy(Store $store)
     {
-
-    }
-
-    public function destroy($id)
-    {
-
+        try{
+            return $store->delete();
+        }catch(\Exception $e){
+            Log::emergency($e->getMessage());
+            return response('can\'t delete this store', 500)
+                ->header('Content-Type', 'text/plain');
+        }
     }
 }
